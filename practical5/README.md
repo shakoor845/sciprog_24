ac6129b268849d8d071ad08803fa1adddb7798f4
saba abdul shakoor <saba.shakoor@tyndall.ie>
2024-10-28 13:31:38 +0000

    practical5

---
+++ practical5/README.md
@@ -0,0 +1,16 @@
+# Practical 5
+
+This folder contains practical 5 codes
+
+
+## Compile
+gcc HyperbolicTangent.c -o hbb -lm
+
+gcc fibonacci.c -o fib
+
+
+##Execute
+
+./hbb
+
+./fib
---
+++ practical5/fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fib(int *a, int *b);
+
+int main(void)
+{
+     int n;
+     printf("please input a positive integer n\n");
+     scanf("%d",&n);
+     if (n<1)
+     {
+       printf("n must be bigger than 1\n");
+       exit(1);
+     }
+     int f0 = 0;
+     int f1 = 1;
+     printf("The fib. series is: \n");
+     printf("%d, %d", f0, f1);
+
+     int i;
+     for (i = 2; i<= n; i++)
+     {
+     fib(&f1, &f0);
+     printf("%d", f1);
+     if ((i + 1) % 10==0)
+{
+printf("\n");
+     }
+     }
+     return 0;
+}
+void fib(int *a, int *b)
+{
+int next;
+next = *a + *b;
+*b = *a;
+*a = next;
+}
